547ae2edeb7a11978163b626ff0ee0f7369cd0b8
Guenter Hack <guenter.hack2@gmail.com>
2026-03-17 14:00:13 +0100

    Ready for QGIS 4.0
    Bugfixes, clean-up and modification of metadata.txt for QGIS 4.0

--- plugin_upload.py
+++ plugin_upload.py
@@ -7,9 +7,13 @@ from __future__ import print_function
 from future import standard_library
 standard_library.install_aliases()
 from builtins import input
+import defusedxml.xmlrpc
 import xmlrpc.client, sys, os
 import getpass
 from optparse import OptionParser
+
+# Monkey patch for xmlrpc as recommended by bandit security scan
+defusedxml.xmlrpc.monkey_patch()
 
 # Configuration
 PROTOCOL='http'
